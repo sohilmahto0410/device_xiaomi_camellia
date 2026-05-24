deae368918aa72d404f0479e46dbe9a42c556c56
Sohil Mahto <52059603+sohilmahto0410@users.noreply.github.com>
2026-05-24 23:35:36 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_waffle.mk
+    $(LOCAL_DIR)/yaap_camellia.mk
 
 COMMON_LUNCH_CHOICES := \
-    yaap_waffle-user \
-    yaap_waffle-userdebug \
-    yaap_waffle-eng
+    yaap_camellia-user \
+    yaap_camellia-userdebug \
+    yaap_camellia-eng
