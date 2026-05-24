f472e5c66c731bf3e9b19d4b22ed526520505616
Sohil Mahto <52059603+sohilmahto0410@users.noreply.github.com>
2026-05-24 23:20:01 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,10 +9,10 @@ git clone  https://github.com/sohilmahto0410/vendor_xiaomi_camellia.git vendor/x
 rm -rf device/mediatek/sepolicy_vndr && git clone  https://github.com/xiaomi-mediatek-devs/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr -b lineage-23.0             
 
 # Hardware Mediatek
-rm -rf hardware/mediatek && git clone https://github.com/LineageOS/android_hardware_mediatek.git  hardware/mediatek
+rm -rf hardware/mediatek && git clone https://github.com/LineageOS/android_hardware_mediatek.git  hardware/mediatek -b lineage-23.2
 
 # Hardware Xiaomi
-rm -rf hardware/xiaomi && git clone  https://github.com/LineageOS/android_hardware_xiaomi.git  hardware/xiaomi
+rm -rf hardware/xiaomi && git clone  https://github.com/LineageOS/android_hardware_xiaomi.git  hardware/xiaomi  -b lineage-23.2
 
 #IMS
 git clone https://github.com/techyminati/android_vendor_mediatek_ims vendor/mediatek/ims/
