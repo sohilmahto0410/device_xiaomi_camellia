291e2e96cd02ad37b71b79a3c141054e05289939
Sohil Mahto <52059603+sohilmahto0410@users.noreply.github.com>
2026-05-24 23:43:41 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,20 @@
 
 # Kernel 
-git clone  https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
+git clone --depth 1 https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
 
 # Device Vendor Tree
-git clone  https://github.com/sohilmahto0410/vendor_xiaomi_camellia.git vendor/xiaomi/camellia 
+git clone --depth 1 https://github.com/sohilmahto0410/vendor_xiaomi_camellia.git vendor/xiaomi/camellia 
 
 # Sepolicies
-rm -rf device/mediatek/sepolicy_vndr && git clone  https://github.com/xiaomi-mediatek-devs/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr -b lineage-23.0             
+rm -rf device/mediatek/sepolicy_vndr && git clone  --depth 1 https://github.com/yaap/device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr -b sixteen            
 
 # Hardware Mediatek
-rm -rf hardware/mediatek && git clone https://github.com/LineageOS/android_hardware_mediatek.git  hardware/mediatek -b lineage-23.2
+rm -rf hardware/mediatek && git clone --depth 1 https://github.com/yaap/hardware_mediatek.git  hardware/mediatek -b sixteen
 
 # Hardware Xiaomi
-rm -rf hardware/xiaomi && git clone  https://github.com/LineageOS/android_hardware_xiaomi.git  hardware/xiaomi  -b lineage-23.2
+rm -rf hardware/xiaomi && git clone --depth 1 https://github.com/yaap/hardware_xiaomi.git  hardware/xiaomi  -b sixteen
 
 #IMS
-git clone https://github.com/techyminati/android_vendor_mediatek_ims vendor/mediatek/ims/
+git clone --depth 1 https://github.com/techyminati/android_vendor_mediatek_ims vendor/mediatek/ims/
 
 
