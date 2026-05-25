344499df860344aa37bb3d1128b3d7fd3df5b0da
Sohil Mahto <52059603+sohilmahto0410@users.noreply.github.com>
2026-05-25 13:29:49 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,10 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_camellia.mk
 
-COMMON_LUNCH_CHOICES := \
-    yaap_camellia-user \
-    yaap_camellia-userdebug \
-    yaap_camellia-eng
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_lineage.mk
