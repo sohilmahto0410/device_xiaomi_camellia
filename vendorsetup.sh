3110e5d9ff120b9c4c9733ae6d454d8a988ab418
Sohil Mahto <52059603+sohilmahto0410@users.noreply.github.com>
2026-05-25 08:59:08 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,11 +5,12 @@ git clone --depth 1 https://github.com/dm700-devs/device_xiaomi_camellia-kernel.
 # Device Vendor Tree
 git clone --depth 1 https://github.com/sohilmahto0410/vendor_xiaomi_camellia.git vendor/xiaomi/camellia 
 
-# Sepolicies
-rm -rf device/mediatek/sepolicy_vndr && git clone  --depth 1 https://github.com/xiaomi-mediatek-devs/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0            
 
 # Hardware Mediatek
 rm -rf hardware/mediatek && git clone --depth 1 https://github.com/yaap/hardware_mediatek.git  hardware/mediatek -b sixteen
+
+# Sepolicies
+rm -rf device/mediatek/sepolicy_vndr && git clone  --depth 1 https://github.com/xiaomi-mediatek-devs/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr -b lineage-23.0            
 
 # Hardware Xiaomi
 rm -rf hardware/xiaomi && git clone --depth 1 https://github.com/yaap/hardware_xiaomi.git  hardware/xiaomi  -b sixteen
