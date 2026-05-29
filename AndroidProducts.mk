67f3d919d4210a4e5461eb85747c05749a20d568
Sohil Mahto <52059603+sohilmahto0410@users.noreply.github.com>
2026-05-29 15:56:13 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,3 +7,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/infinity_camellia.mk
+
+COMMON_LUNCH_CHOICES := \
+    infinity_camellia-user \
+    infinity_camellia-userdebug \
+    infinity_camellia-eng
